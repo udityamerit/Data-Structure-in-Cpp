f1ae63f619208a88e0f486baa609a2f3375ae2ce
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-10-05 22:19:34 +0530

    Updating the code of insert at middle node

--- 3 LinkedList/1 Single LinkedList/3_insert_at_middle.cpp
+++ 3 LinkedList/1 Single LinkedList/3_insert_at_middle.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 
-// create a node:
+// create a Node:
 struct Node{
         public:
         int data;
         Node* next;
 
-        // creating the constructor
+        // creating a constructor
         Node(int data){
                 this->data = data;
                 this->next = NULL;
@@ -16,12 +16,54 @@ struct Node{
 };
 
 
-// intert the node at head:
+// insert a node at head:
 void insertathead(Node* &head, int dataa){
         Node* temp = new Node(dataa);
         temp->next = head;
         head = temp;
+        
 }
+
+// insert a node at the end:
+void insertatlast(Node* &tail, int da){
+        Node* temp  = new Node(da);
+        tail->next = temp;
+        tail = tail->next;
+
+}
+
+// insert a node at the middle of the linkedlist using the position:
+void insertatmiddle(int position, int d, Node* head, Node* tail){
+
+        // insert at start
+        if (position==1){
+                insertathead(head, d);
+                return;
+        }
+        int cnt = 1;
+
+        Node* temp = head;
+
+        // updating the head
+        while(cnt<position-1){
+                temp = temp->next;
+                cnt++;
+        }
+
+        // inserting at last position
+        if (temp->next==NULL){
+                insertatlast(tail, d);
+                return;
+        }
+
+        // create a node which is insert at the middle
+        Node* middlenode = new Node(d);
+        middlenode->next = temp->next; 
+        temp->next = middlenode;
+
+}
+
+
 
 // creating a function to show the data of linkedlist:
 void printdata(Node* &head){
@@ -30,19 +72,37 @@ void printdata(Node* &head){
                 cout<<temp->data<<" ";
                 temp = temp->next;
         }
+        cout<<endl;
 }
 int main(){
 
         // creating a node
         Node* node1 = new Node(10);
+
+        // creating a head pointer for node1
         Node* head = node1;
+
+        // creating a tail pointer for node1
+        Node* tail = node1;
         // cout<<node1->data;
 
         // insert the node;
         insertathead(head, 40);
         printdata(head);
 
+        // insert a node at last
+        insertatlast(tail, 90);
+        printdata(head);
 
+        // insert a node at 3rd position
+        insertatmiddle(3, 22, head, tail);
+        printdata(head);
+
+        // insertatmiddle(4, 90, head, tail);
+        // printdata(head);
+
+        cout<<"head = "<<head->data<<endl;
+        cout<<"tail = "<<tail->data<<endl;
 
         return 0;
 }
