9931099556aa47f36f1c11f1a924fbf1b0c0b4ec
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-10-05 21:16:54 +0530

    update the file of linkedlist

---
+++ 3 LinkedList/1 Single LinkedList/3_insert_at_middle.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// create a node:
+struct Node{
+        public:
+        int data;
+        Node* next;
+
+        // creating the constructor
+        Node(int data){
+                this->data = data;
+                this->next = NULL;
+        }
+};
+
+
+int main(){
+
+
+
+        return 0;
+}
