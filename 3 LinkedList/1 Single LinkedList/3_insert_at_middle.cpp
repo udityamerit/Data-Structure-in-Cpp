559532cad28f53a907de3b6b5631ac94d6829579
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-10-05 21:27:26 +0530

    update

--- 3 LinkedList/1 Single LinkedList/3_insert_at_middle.cpp
+++ 3 LinkedList/1 Single LinkedList/3_insert_at_middle.cpp
@@ -16,7 +16,31 @@ struct Node{
 };
 
 
+// intert the node at head:
+void insertathead(Node* &head, int dataa){
+        Node* temp = new Node(dataa);
+        temp->next = head;
+        head = temp;
+}
+
+// creating a function to show the data of linkedlist:
+void printdata(Node* &head){
+        Node* temp = head;
+        while(temp!=NULL){
+                cout<<temp->data<<" ";
+                temp = temp->next;
+        }
+}
 int main(){
+
+        // creating a node
+        Node* node1 = new Node(10);
+        Node* head = node1;
+        // cout<<node1->data;
+
+        // insert the node;
+        insertathead(head, 40);
+        printdata(head);
 
 
 
