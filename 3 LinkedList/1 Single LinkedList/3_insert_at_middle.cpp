310523db4eb6ed54d2c487e7a226ad7122bf3674
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-10-05 22:45:03 +0530

    compliting the singlily linkedlist

--- 3 LinkedList/1 Single LinkedList/3_insert_at_middle.cpp
+++ 3 LinkedList/1 Single LinkedList/3_insert_at_middle.cpp
@@ -13,6 +13,17 @@ struct Node{
                 this->data = data;
                 this->next = NULL;
         }
+
+        // creating a destructior
+        ~Node(){
+                int value = this->data;
+                // memory free
+                if(this->next != NULL){
+                        delete next;
+                        this->next = NULL;
+                }
+                cout<<"memory is free for node with data: "<<value<<endl;
+        }
 };
 
 
@@ -64,6 +75,33 @@ void insertatmiddle(int position, int d, Node* head, Node* tail){
 }
 
 
+// Deleting the node:
+void deletnode(int position, Node* &head){
+        // deleting first or start node
+        if(position==1){
+                Node* temp = head;
+                head = head->next;
+                // free the memory
+                temp->next = NULL;
+                delete temp;
+        }
+        else{
+                // deleting any middle node or last node
+                Node* curr = head;
+                Node* prev = NULL;
+
+                int cnt = 1;
+                while(cnt < position){
+                        prev = curr;
+                        curr = curr->next;
+                        cnt++;
+                }
+                prev->next = curr->next;
+                curr->next = NULL;
+                delete curr;
+        }
+}
+
 
 // creating a function to show the data of linkedlist:
 void printdata(Node* &head){
@@ -103,6 +141,11 @@ int main(){
 
         cout<<"head = "<<head->data<<endl;
         cout<<"tail = "<<tail->data<<endl;
+
+        // deleting the node
+        deletnode(3,head);
+        printdata(head);
+
 
         return 0;
 }
