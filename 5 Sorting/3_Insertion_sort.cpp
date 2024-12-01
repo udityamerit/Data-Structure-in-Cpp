3aa4a82f80f001934cf38e0810d8910cc7f374d3
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-12-01 22:24:22 +0530

    Adding the 3rd type of sorting algorithm insertion sort

---
+++ 5 Sorting/3_Insertion_sort.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertionsort(int arr[], int n){
+        for(int i = 1; i<n; i++){
+                int curr = arr[i];
+                int prev = i-1;
+
+                while(prev>=0 && arr[prev]>curr){
+                        arr[prev+1] = arr[prev];
+                        prev--;
+                }
+                arr[prev+1] = curr;
+        }
+}
+
+void printarr(int arr[] , int n){
+        for(int i = 0 ; i<n; i++){
+                cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+}
+
+int main(){
+        int n = 5;
+        int arr[] = {23,12,45,32,12};
+        insertionsort(arr, n);
+        printarr(arr, n);
+        return 0;
+}
