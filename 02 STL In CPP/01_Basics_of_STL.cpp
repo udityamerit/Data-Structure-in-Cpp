9e68c7e6023303b7e7584359da725080758ecd0f
Uditya Narayan Tiwari <uditmerit@gmail.com>
2025-12-18 00:21:15 +0530

    updating the codes for basics of stl and its types

---
+++ 02 STL In CPP/01_Basics_of_STL.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+/*
+
+STL(Standard Template Library --> it is predefines libraries which is used for saving the processing powers and optimizing the code complexity and improve the code readably
+
+
+Components of STL:
+
+1. Containers --> store the data using the templates
+2. Algorithms --> sorting, searching
+3. Iterators --> it is a object that points the object in the containers (like pointers )
+
+
+STL = containers(object which store the data ) + Algorithms(procedure to process the data) + Iterators(object which points to an element of a containers )
+
+
+Containers--> it is 3 types of :
+        1. Sequence Containers --> store the data in linear fashion.
+                1.1 vector--> random access-->Fast, insertion/deletion-->slow
+                1.2 list--> random access-->slow, insertion/deletion-->Fast, 
+                1.3 double ended queue
+
+        2. Associative Containers --> direct access ( set, map, multiset, multimap, tree structure like storage )
+                --> all the operations are fast Except Random Access
+
+        3. Derived Containers --> real world Modeling ( queue, stack, priority queue )
+                --> Depends on the Data Structure
+
+*/
+
+int main(){
+
+
+        return 0;
+}
