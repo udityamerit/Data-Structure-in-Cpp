2d5e201cb62d77bfa1d49201e128f6fc2fc599b9
Uditya Narayan Tiwari <uditmerit@gmail.com>
2025-12-18 00:02:42 +0530

    intro of STL in cpp

---
+++ 02 STL In CPP/01_Containers_STL.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+/*
+Components of STL:
+
+1. Containers --> store the data using the templates
+2. Algorithms --> sorting, searching
+3. Iterators --> it is a object that points the object in the containers (like pointers )
+
+
+
+*/
+
+int main(){
+
+
+        return 0;
+}
