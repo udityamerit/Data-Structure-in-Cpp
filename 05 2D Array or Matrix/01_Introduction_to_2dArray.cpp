664ff046da9d04531d17d50272b08645892d0dd1
Uditya Narayan Tiwari <uditmerit@gmail.com>
2025-12-24 23:52:21 +0530

    updating the code of 2d matrix

--- 05 2D Array or Matrix/01_Introduction_to_2dArray.cpp
+++ 05 2D Array or Matrix/01_Introduction_to_2dArray.cpp
@@ -3,6 +3,32 @@
 using namespace std;
 
 int main(){
+        int arr[10][10];
+        int m,n;
+        cin>>m>>n;
+        for(int i = 0; i<m; i++){
+                for(int j=0; j<n; j++){
+                        cin>>arr[i][j];
+                }
+                
+        }
+        //output for 2d matrix; row wise printing
+        cout<<"Row wise printing"<<endl;
+        for(int i = 0; i<m; i++){
+                for(int j=0; j<n; j++){
+                        cout<<arr[i][j]<<" ";
+                }
+                cout<<endl;
+        }
+
+        cout<<"column wise printing"<<endl;
+
+        for(int i = 0; i<n; i++){
+                for(int j=0; j<m; j++){
+                        cout<<arr[j][i]<<" ";
+                }
+                cout<<endl;
+        }
         
         return 0;
 }
