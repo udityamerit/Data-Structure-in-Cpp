abfe09093b091abf0d47b85dd9a490e8aa29857d
Uditya Narayan Tiwari <uditmerit@gmail.com>
2026-01-23 23:06:43 +0530

    update the code

--- 02 STL In CPP/05_Functions_STL.cpp
+++ 02 STL In CPP/05_Functions_STL.cpp
@@ -7,8 +7,10 @@ using namespace std;
 int main(){
 
         // Functional Objects(Functor): Function wrapped in a class so that it available like an object
+        
         int arr[] = {1, 45, 23, 65, 6, 2};
         sort(arr, arr+4); // it is sort the elements from 0 to 4th index in the ascending order
+
         sort(arr,arr+6, greater<int>());  // it is sort the elements from 0 to 4th index in the descending order
 
         for(int i  = 0; i<6; i++){
