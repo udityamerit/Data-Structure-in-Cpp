3f61bdd2b32e95453ee1dd191f93a805b82c7a91
Uditya Narayan Tiwari <uditmerit@gmail.com>
2026-05-03 11:58:35 +0530

    updatet he code of string

--- 06 String/String.cpp
+++ 06 String/String.cpp
@@ -2,10 +2,30 @@
 #include<string>
 #include<cmath>
 #include<algorithm>
+#include<vector>
 
 using namespace std;
 
 int main(){
+        // initializing the string statically
         string sc;
+        sc = "hi i am a student";
+        cout<<sc<<endl;
+
+        string s = "hello";
+        cout<<s<<endl;
+        
+        // initializing the string dynamically
+        string *sp = new string;
+        *sp = "Hello i am dynamic";
+        cout<<sp<<endl;
+        cout<<*sp<<endl;
+
+        // using the vectors to initializing the 2d string using the vectors
+        vector<string> v;
+        v.push_back("mohan");
+        v.push_back("ramesh");
+        
+
         return 0;
 }
