81d67c6381573a038d9184a19c82915b7b8eb677
Uditya Narayan Tiwari <uditmerit@gmail.com>
2026-05-14 21:37:32 +0530

    update the strings

--- 06 String/String.cpp
+++ 06 String/String.cpp
@@ -23,9 +23,32 @@ int main(){
 
         // using the vectors to initializing the 2d string using the vectors
         vector<string> v;
+        vector<string> v1;
         v.push_back("mohan");
         v.push_back("ramesh");
+        for(int i=0; i<v.size(); i++){
+                cout<<v[i]<<endl;
+                sort(v[i].begin(), v[i].end());
+                cout<<v[i]<<endl;
+        }
         
+        //geting the input from the user
+        string input_string;
+        // getline(cin,input_string);
+        // cout<<input_string<<endl;
 
+        // concatenate the strings
+
+        string ne_s = s+sc+*sp;
+        cout<<ne_s<<endl;
+
+        // using the substrings
+        string sub = "hi,Hello?";
+        cout<<sub.substr(3)<<endl;
+
+        //using the loop to print the all substrings
+        for(int i = 0 ; i<sub.size(); i++){
+                for(int j = )
+        }
         return 0;
 }
